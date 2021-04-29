b5cf297461b9f82aa19ad68154e9a3e5cc2e6e2a
RonaldoCaetano <ronaldogodoi2@gmail.com>
2021-04-29 19:42:32 -0300

    adding content loader

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useMemo, useState, useCallback } from "react";
 import axios from "axios";
+import ContentLoader from "react-content-loader";
 
 import ProductCard from "../ProductCard";
+
 import * as S from "./styles";
 
 type ProductListProps = {
@@ -10,6 +12,7 @@ type ProductListProps = {
 
 const ProductList = ({ products }: ProductListProps): React.ReactElement => {
   const [productsData, setProductsData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const getProductOnWishlist = useCallback((productData: Product) => {
     const localWishlist: Product[] = JSON.parse(localStorage.getItem("wishlist"));
@@ -32,21 +35,40 @@ const ProductList = ({ products }: ProductListProps): React.ReactElement => {
         .then(({ data }) => {
           if (data?.products?.length) {
             setProductsData(data.products);
+            setLoading(false);
           }
         });
+    } else {
+      setLoading(false);
     }
   }, [products]);
 
   return (
-    <S.ProductsContainer data-testid="products-list">
-      {productsToShow.map((product, index) => (
-        <ProductCard
-          key={index}
-          getProductOnWishlist={getProductOnWishlist}
-          productData={product}
-        />
-      ))}
-    </S.ProductsContainer>
+    <>
+      {loading ? (
+        <S.ProductsContainer>
+          <ContentLoader
+            width="100%"
+            height={600}
+            speed={3}
+            backgroundColor="#f3f3f3"
+            foregroundColor="#ecebeb"
+          >
+            <rect x="0" y="0" rx="2" ry="2" width="100%" height="100%" />
+          </ContentLoader>
+        </S.ProductsContainer>
+      ) : (
+        <S.ProductsContainer data-testid="products-list">
+          {productsToShow.map((product, index) => (
+            <ProductCard
+              key={index}
+              getProductOnWishlist={getProductOnWishlist}
+              productData={product}
+            />
+          ))}
+        </S.ProductsContainer>
+      )}
+    </>
   );
 };
 
